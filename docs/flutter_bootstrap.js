09b45114c25bea5ddee7336fc953014921e7f237
Yuheiuem <yuuheipc@gmail.com>
2026-07-08 11:04:36 +0900

    公開ページを更新

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "103595510" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3885441111" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
